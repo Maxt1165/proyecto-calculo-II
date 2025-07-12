6a413df2de3a350af5bbb2be20a2bcece00fedbb
Maxt1165 <maoa.olayanastacio@gmail.com>
2025-07-12 16:27:25 -0500

    TODO LISTO PARA PROGRAMAR

--- __init__.py
+++ __init__.py
@@ -17,12 +17,14 @@ print("Se importó bpy en el archivo __init__.py")
 #def unregister():
    # ui_panel.unregister()
 
-# ESTO ACTUALIZARA LOS MODULOS (ARCHIVOS) CADA QUE SE RECARGA ESTE __init__.py
+# ESTO ACTUALIZARÁ LOS MÓDULOS (ARCHIVOS) CADA QUE SE RECARGUE ESTE __init__.py
 import importlib
-print("Se importó importlib en el archivo __init__.py")
 from . import ui_panel
-print("Se importó ui_panel en el archivo __init__.py")
-_modules = [ui_panel]
+from . import opB_superficie
+# from . import opB_gradiente
+# from . import propiedades
+
+_modules = [ui_panel, opB_superficie]  # Añade aquí más módulos si es necesario
 
 def _reload_modules():
     for m in _modules:
@@ -30,46 +32,12 @@ def _reload_modules():
 
 def register():
     _reload_modules()
-    
-    # Registrar módulos base (como ui_panel)
+
     for m in _modules:
         if hasattr(m, "register"):
             m.register()
 
-    # Importar componentes después del arranque (evita ciclos)
-    from . import opB_superficie
-    print("Se importó opB_superficie en el archivo __init__.py")
-
-
-    # 1. Propiedades
-
-
-    # 2. Operadores
-    #operacionesBlender.superficie_opB.register()
-    #operacionesBlender.gradiente_opB.register()
-    opB_superficie.register()
-
-    # 3. Panel UI
-    #ui_panel.register()
-
-    # 4. Inicializar adaptador o servicios
-
-
-
 def unregister():
-    from . import opB_superficie
-    print("Se importó opB_superficie en el archivo __init__.py")
-    # Desregistrar en orden inverso
-    #ui_panel.unregister()
-    #operacionesBlender.ptangente_opB.unregister()
-    #operacionesBlender.gradiente_opB.unregister()
-    opB_superficie.unregister()
-    
-
-    # Si hay que cerrar algo del adaptador, sería aquí
-
-    # Finalmente, desregistrar ui_panel y otros módulos
     for m in reversed(_modules):
         if hasattr(m, "unregister"):
             m.unregister()
-
