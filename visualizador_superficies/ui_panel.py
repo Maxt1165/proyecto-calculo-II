a022c5034f708d22e46e98d7b88ee02403aae592
Maxt1165 <maoa.olayanastacio@gmail.com>
2025-07-11 19:08:42 -0500

    Update ui_panel.py

--- visualizador_superficies/ui_panel.py
+++ visualizador_superficies/ui_panel.py
@@ -1,18 +1,18 @@
 import bpy
 
-class VISUALIZADOR_PT_MainPanel(bpy.types.Panel):
-    bl_label = "Visualizador de Superficies"
-    bl_idname = "VISUALIZADOR_PT_main"
+class VISUALIZADOR_PT_Panel(bpy.types.Panel):
+    bl_label = "Panel de Prueba"
+    bl_idname = "VISUALIZADOR_PT_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'Superficies'
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Panel listo para comenzar")
+        layout.label(text="¡Hola desde el Add-on!")
 
 def register():
-    bpy.utils.register_class(VISUALIZADOR_PT_MainPanel)
+    bpy.utils.register_class(VISUALIZADOR_PT_Panel)
 
 def unregister():
-    bpy.utils.unregister_class(VISUALIZADOR_PT_MainPanel)
+    bpy.utils.unregister_class(VISUALIZADOR_PT_Panel)
