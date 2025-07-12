2d6be2f8385b46b28b77be30eedb059e533ecadf
LeslyLotusPhoneix <leslymunozalberca@gmail.com>
2025-07-11 22:53:48 -0500

    Update superficie_opB.py

--- proyecto-calculo-II/operacionesBlender/superficie_opB.py
+++ proyecto-calculo-II/operacionesBlender/superficie_opB.py
@@ -2,12 +2,12 @@
 
 #Dada una función matemática z = f(x, y), generar una una malla (grid) que represente esa superficie en el espacio 3D.
 """PASOS
-1. Definir la función: El usuario ingresa una cadena (string) con la expresión matemática, por ejemplo: `"x**2 + y**2"`.
-2. Definir el dominio: Rango para `x` y `y` (por ejemplo, `x` de -5 a 5, `y` de -5 a 5) y la resolución (número de puntos).
-3. Evaluar la función: Para cada punto `(x, y)` en el dominio, calcular `z = f(x, y)`.
+1. Definir la función: El usuario ingresa una cadena (string) con la expresión matemática, por ejemplo: "x**2 + y**2".
+2. Definir el dominio: Rango para x y y (por ejemplo, x de -5 a 5, y de -5 a 5) y la resolución (número de puntos).
+3. Evaluar la función: Para cada punto (x, y) en el dominio, calcular z = f(x, y).
 4. Crear la malla: Construir una malla de vértices y caras (quadrados o triángulos) que represente la superficie.
 5. Añadir el objeto a la escena de Blender """
-
+import bpy
 from ..logica import superficie_generar
 from ..utilidades import manejoErrores
 
@@ -15,7 +15,9 @@ class CrearSuperficies(bpy.types.Operator):
     bl_idname = "visualizador_superficies.crear_superficie"
     bl_label = "Crear Superficie"
     bl_options = {'REGISTER', 'UNDO'}
-    
+    #REGISTER: Hace visible el operador en la UI y permite asignar atajos de teclado.
+    #Permite deshacer la acción con Ctrl+Z.
+
     @classmethod
     #Verifica si el operador puede ejecutarse en el contexto actual
     def poll(cls, context):
@@ -55,6 +57,5 @@ class CrearSuperficies(bpy.types.Operator):
             self.report({'ERROR'}, f"Error: {str(e)}")  # Muestra error en la UI
             return {'CANCELLED'}  # Indica que la operación falló
             
-    import bpy
 
   
