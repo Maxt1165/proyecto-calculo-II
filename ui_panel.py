1553b317fdedf056549e4534f995ea2cedda6d80
LeslyLotusPhoneix <leslymunozalberca@gmail.com>
2025-07-11 23:37:00 -0500

    :3

--- ui_panel.py
+++ ui_panel.py
@@ -1,4 +1,5 @@
 import bpy
+from . import gui
 
 class VISUALIZADOR_PT_Panel(bpy.types.Panel):
     bl_label = "Panel de Prueba"
@@ -11,8 +12,16 @@ class VISUALIZADOR_PT_Panel(bpy.types.Panel):
         layout = self.layout
         layout.label(text="GANARON LOS MALOS 🗣️")
 
+
+#Registro del panel CALCBLENDER_PT_SurfacePanel
 def register():
+    # Registra tu panel principal de superficies
+    bpy.utils.register_class(gui.CALCBLENDER_PT_SurfacePanel)
+    
+    # Registra este panel adicional si lo necesitas
     bpy.utils.register_class(VISUALIZADOR_PT_Panel)
 
 def unregister():
+    # Desregistra en orden inverso
     bpy.utils.unregister_class(VISUALIZADOR_PT_Panel)
+    bpy.utils.unregister_class(gui.CALCBLENDER_PT_SurfacePanel)
