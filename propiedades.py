f081b7d32224f7b4a2d38a74000462951569b480
Maxt1165 <maoa.olayanastacio@gmail.com>
2025-07-12 16:58:52 -0500

    a

--- propiedades.py
+++
@@ -1,9 +0,0 @@
-import bpy
-
-class CalcBlenderProperties(bpy.types.PropertyGroup):
-    superficie_funcion: bpy.props.StringProperty(name="Función z =")
-    superficie_x_min: bpy.props.FloatProperty(name="X Min", default=-5)
-    superficie_x_max: bpy.props.FloatProperty(name="X Max", default=5)
-    superficie_y_min: bpy.props.FloatProperty(name="Y Min", default=-5)
-    superficie_y_max: bpy.props.FloatProperty(name="Y Max", default=5)
-    superficie_resolution: bpy.props.IntProperty(name="Resolución", default=10, min=3, max=30)
